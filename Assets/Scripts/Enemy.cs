201ccffba4828cd938ad63e581f1bc5ac319af50
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-05-24 22:51:06 +0900

    add enemy collision and destroy system

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float verticalLimit = -5f;
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < verticalLimit)
+        {
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+        else if (other.gameObject.tag.Equals("Laser"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
