9f4596a7ad003331e6947628c4a3736219e69645
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-05-24 18:53:18 +0900

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+
+        float horizontalLimit = -3.8f;
+        float verticalLimit = 11.3f;
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, horizontalLimit, 0), 0);
+
+        if (transform.position.x > verticalLimit)
+        {
+            transform.position = new Vector3(-verticalLimit, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -verticalLimit)
+        {
+            transform.position = new Vector3(verticalLimit, transform.position.y, 0);
+        }
+    }
+}
